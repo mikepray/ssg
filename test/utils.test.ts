ab0c68670dba52b541cf09284682cadc6a3c51b0
mike.pray <mwpray@gmail.com>
2022-09-22 15:18:04 -0500

    more test

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -1,9 +1,10 @@
 import chalk from "chalk";
 import tap from "tap";
 import { testingStationState } from "../src/data/testStartingState";
+import { vessels } from "../src/data/vessels";
 import { printStationStatus } from "../src/game";
 import { Log } from "../src/types";
-import { getAssignedCrew, getUnassignedCrew, progressBar } from "../src/utils";
+import { getAssignedCrew, getUnassignedCrew, getVesselColor, progressBar } from "../src/utils";
 
 
 const stationState = testingStationState;
@@ -18,3 +19,7 @@ tap.matchSnapshot(progressBar(100, 50, 100, chalk.white, chalk.black), 'progress
 
 tap.equal(getAssignedCrew(stationState), 5);
 tap.equal(getUnassignedCrew(stationState), 0);
+
+tap.equal(getVesselColor(undefined, stationState.factions), 'FFFFFF');
+tap.equal(getVesselColor(vessels.find(vessel => vessel.name === 'Zeelandia'), stationState.factions), 'F5A623');
+tap.equal(getVesselColor(vessels.find(vessel => vessel.name === 'NoSuchShip'), stationState.factions), 'FFFFFF');
